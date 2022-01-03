b8bc39f35a1eb5dc6e612410553d5c6627a156b9
Chris Grabosky <chris@grabosky.net>
2022-01-03 09:14:13 -0500

    group by prop color

--- MonopoSaver/DataModels/Property.cs
+++ MonopoSaver/DataModels/Property.cs
@@ -5,10 +5,12 @@ namespace MonopoSaver.DataModels {
         public int isMortgaged {get;set;}
         public int Houses {get;set;}
         public bool Ownable {get;set;}
+        public string ColorName {get;set;}
 
         public Prop() {
             Name = "";
             Color = "#000000";
+            ColorName = "Black";
             isMortgaged = 0;
             Houses = 0;
             Ownable = true;
